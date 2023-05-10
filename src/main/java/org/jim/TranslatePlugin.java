882cca90476b873507b92d08f37eb47565abb9f7
yangxin <943332358@.qq.com>
2023-05-10 22:01:00 +0800

    增加gui面板标题和图标信息

--- src/main/java/org/jim/TranslatePlugin.java
+++ src/main/java/org/jim/TranslatePlugin.java
@@ -4,8 +4,22 @@ package org.jim;
  * @author jim
  */
 public interface TranslatePlugin {
-    String translate(String sourceText, String sourceLanguage, String targetLanguage);
-
 
     String translate(String sourceText);
+
+
+    /**
+     * 插件显示面板的标题，默认为空
+     */
+    default String translatePaneTitle() {
+        return "";
+    }
+
+
+    /**
+     * 插件显示面板的图标，默认为空。读取路径为src/main/resources/img/
+     */
+    default String translatePaneIconPath() {
+        return null;
+    }
 }
